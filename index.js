2a479295e424af7e6e958b15ed35d6c40354a4e6
yrjwcharm <xlyanrui@sina.com>
2018-09-07 18:25:56 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import ActionSheet from './app/components/ActionSheet'
+import Flex from './app/components/Flex';
+import WingBlank from './app/components/WingBlank';
+import WhiteSpace from './app/components/WhiteSpace';
+import Drawer from './app/components/Drawer';
+import Popover from './app/components/Popover';
+import Pagination from './app/components/Pagination';//分页器
+import SegmentedControl from './app/components/SegmentedControl';//分段器
+//import Tab from './app/components/Tab';//分段器    ......缺少图片
+//import Tabbar from './app/components/Tabbar';//分段器   ......缺少图片
+
+//AppRegistry.registerComponent('Example', () => App);
+//AppRegistry.registerComponent('Example', () => ActionSheet);
+//AppRegistry.registerComponent('Example', () => Flex);
+//AppRegistry.registerComponent('Example', () => WingBlank);
+//AppRegistry.registerComponent('Example', () => WhiteSpace);
+//AppRegistry.registerComponent('Example', () => Drawer);
+//AppRegistry.registerComponent('Example', () => Popover);
+//AppRegistry.registerComponent('Example', () => Pagination);
+//AppRegistry.registerComponent('Example', () => SegmentedControl);
+//AppRegistry.registerComponent('Example', () => Tab);
+//AppRegistry.registerComponent('Example', () => TabBar);
+//AppRegistry.registerComponent('Example', () => SegmentedControl);
